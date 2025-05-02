bd6f2109b892a45a8bea91a07dae4a3fd204db30
Deekayee <nesterenko91@gmail.com>
2025-05-03 00:11:47 +0100

    removed unnecessary includes

--- shop.cpp
+++ shop.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <string>
-#include <cctype>
 #include "utils.h"
 #include "menu.h"
-using namespace std;
 
 int main()
 {
     // Data init
     vector<Stock> stockList;
 
-    // exit if data loading failed
+    // Exit if data loading failed
     if (dataInit(stockList) != 0)
-        return 1; 
+        return 1;
 
     // Main menu
     mainMenu(stockList);
