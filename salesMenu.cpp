cf96c8212f43c6f3c171f3d8146f8f90d82cbcf9
xd4rking <rui.santos.maia@gmail.com>
2025-05-06 12:55:22 +0100

    Init commit for sale

--- salesMenu.cpp
+++ salesMenu.cpp
@@ -5,11 +5,7 @@ using namespace std;
 
 
 // Sales Menu
-<<<<<<< HEAD
 void salesMenu(vector <Stock> &stockList, vector <CartItem> &cart)
-=======
-void salesMenu()
->>>>>>> e721a0d3870264362d948158f1876a54a753344f
 {
     bool salesMenu = true;
     string input;
@@ -18,7 +14,6 @@ void salesMenu()
     {
         do
         {
-<<<<<<< HEAD
             
             
             limh();
@@ -27,30 +22,12 @@ void salesMenu()
                  << "2. View Cart" << endl;
             cout << "Option: ";
 
-=======
-            clearConsole();
-            setColor("\033[0;36m");
-            cout << "Sales Menu" << endl;
-            setColor("\033[0m");
-            limh();
-            cout << "1. Show Products" << endl;
-            limh();
-            cout << "2. Go Back" << endl;
-            limh();
-            cout << "Option: ";
->>>>>>> e721a0d3870264362d948158f1876a54a753344f
             getline(cin, input);
         } while (!validateMenuInput(input, salesOpt));
 
         switch (salesOpt)
         {
         case 1:
-<<<<<<< HEAD
-=======
-            cout << "Products" << endl;
-            limh();
-            // show products
->>>>>>> e721a0d3870264362d948158f1876a54a753344f
             productsMenu();
             break;
         case 2:
@@ -66,20 +43,12 @@ void salesMenu()
 }
 
 // Products Menu
-<<<<<<< HEAD
 int productsMenu() // TODO
-=======
-void productsMenu() // TODO
->>>>>>> e721a0d3870264362d948158f1876a54a753344f
 {
     bool productsMenu = true;
     int productsOpt;
     do
     {
-<<<<<<< HEAD
-=======
-        clearConsole();
->>>>>>> e721a0d3870264362d948158f1876a54a753344f
 
         // function to show products as selectable options, when selected, show more details and give option to add to cart (input quantity here), checkout or cancel
         cout << "Products" << endl;
