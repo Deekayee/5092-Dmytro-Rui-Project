463d7c141dc8ac59e26fb660e5274890de7d630e
xd4rking <rui.santos.maia@gmail.com>
2025-05-12 02:45:21 +0100

    JACKPOT DEVELOPED

--- salesMenu.cpp
+++ salesMenu.cpp
@@ -408,14 +408,23 @@ void checkoutMenu(vector<Stock> &stockList, vector<Stock> &shelf, vector<CartIte
 void gambling(Receipt &sale, int chance)
 {
     srand(time(0));
-    int badLuck = (rand() % 101); // luck goes from 0 to 100
-    if (badLuck < chance)
+    int badLuck = (rand() % 101); // badluck goes from 0 to 100
+
+    if (badLuck < chance)         // if the "bad luck" is less than the chance, win
     {
         vector<CartItem> roster = sale.getItems();
 
-        srand(time(0));
-        int sortedIndex = rand() % roster.size();
-        
+        srand(time(0));                           // reinitializing seed
+        int sortedIndex = rand() % roster.size(); // randomizing index
+
+        CartItem jackpot = roster.at(sortedIndex);
+        cout << "You won a free " << jackpot.getProductName() << "!" << endl;
+        jackpot.setQuantity(1); // client only gets one, >:(
+
+        int resetPrice = 0 - jackpot.getSaleWithoutTax(); //price is negative to subtract total
+        jackpot.setSaleWithoutTax(resetPrice);
+
+        sale.getItems().push_back(jackpot); //item gets added at end of receipt for display
     }
 }
 
