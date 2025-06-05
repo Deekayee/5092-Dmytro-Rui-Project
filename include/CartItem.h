233f08c684c9b7ff990c4d92eb67a5322a6a3ef9
Deekayee <nesterenko91@gmail.com>
2025-06-05 20:24:58 +0100

    changed up cart item header

--- include/CartItem.h
+++ include/CartItem.h
@@ -1,15 +1,7 @@
 #pragma once
-#include "../include/Stock.h"
 
-#include <iostream> // for cout
-#include <string>   // for string
-#include <ctime>    // for time
-#include <sstream>  // for stringstream
-#include <iomanip>  // for setw
-#include <vector>   // for vectors
-#include <cmath>    // for rounding doubles
-
-
+#include <string>
+#include "stock.h"
 
 class CartItem
 {
@@ -21,38 +13,29 @@ private:
     string productName;
 
 public:
-    // Default constructor
+    // Constructors
     CartItem();
-
-    // Construct a CartItem from a Stock item, quantity and tax rate (default = 23%)
     CartItem(const Stock &stock, int qty, double taxRate = 23.0);
 
-    int getStockId() const;
-    int getQuantity() const;
-    double getSaleWithoutTax() const;
-    double getTaxRatePercent() const;
-    string getProductName() const;
+    // Getters
+    const int getStockId();
+    const int getQuantity();
+    const double getSaleWithoutTax();
+    const double getTaxRatePercent();
+    const string &getProductName();
+    const double getSaleWithTax();
+    const double getTotalSaleWithoutTax();
+    const double getTotalItemSellValue();
 
+    // Setters
     void setStockId(int id);
     void setQuantity(int qty);
     void setSaleWithoutTax(double price);
     void setTaxRatePercent(double rate);
-    void setProductName(string name);
+    void setProductName(string &name);
 
-    // Calculates and returns the sale value of the item including tax by applying the tax rate to the sale value without tax.
-    double getSaleWithTax() const;
-
-    // Returns the total value of the item in the cart, without tax.
-    double getTotalSaleWithoutTax() const;
-
-    // Returns the total value of the item in the cart, including tax.
-    double getTotalItemSellValue() const;
-
-    // Returns a string with the CartItem attributes in CSV format.
+    // Methods
     string toString() const;
-
     string toDisplay() const;
-
-    // Parses a comma-separated string to initialize CartItem attributes.
-    void fromString(string line);
+    void fromString(const string &line);
 };
