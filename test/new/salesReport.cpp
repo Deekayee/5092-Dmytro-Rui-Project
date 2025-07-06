28dd8fe037d3af88b1c014f8d52d547aa914a2f2
Deekayee <dmytro.k.nesterenko@gmail.com>
2025-07-06 19:34:12 +0100

    reporting for duty

---
+++ test/new/salesReport.cpp
@@ -0,0 +1,199 @@
+#include "salesReport.h"
+#include "cartItem.h"
+
+#include <climits>
+
+void SalesReport::loadReceipts(const array<Receipt, 100>& saleList)
+{
+    receipts = saleList;
+    receiptCount = countValidReceipts();
+}
+
+void SalesReport::loadStock(const vector<Stock>& stock)
+{
+    stockList = stock;
+    maxStockId = stockList.size(); // Since IDs go from 1 to size
+}
+
+int SalesReport::countValidReceipts() const
+{
+    int count = 0;
+    for (int i = 0; i < 100; i++) {
+        if (receipts[i].getReceiptId() > 0) { // Assuming valid receipts have ID > 0
+            count++;
+        }
+    }
+    return count;
+}
+
+int SalesReport::getTopClientByValue() const
+{
+    int topClient = -1;
+    double maxAmount = 0;
+    
+    // Check each receipt to find unique clients
+    for (int i = 0; i < receiptCount; i++) {
+        if (receipts[i].getReceiptId() > 0) {
+            int currentClient = receipts[i].getClientId();
+            double clientTotal = getClientTotalPurchases(currentClient);
+            
+            if (clientTotal > maxAmount) {
+                maxAmount = clientTotal;
+                topClient = currentClient;
+            }
+        }
+    }
+    
+    return topClient;
+}
+
+int SalesReport::getMostSoldProductByQuantity() const
+{
+    int mostSoldProduct = -1;
+    int maxQuantity = 0;
+    
+    // Check each product ID from 1 to maxStockId
+    for (int productId = 1; productId <= maxStockId; productId++) {
+        int totalQuantity = getProductTotalQuantitySold(productId);
+        
+        if (totalQuantity > maxQuantity) {
+            maxQuantity = totalQuantity;
+            mostSoldProduct = productId;
+        }
+    }
+    
+    return mostSoldProduct;
+}
+
+int SalesReport::getLeastSoldProductByQuantity() const
+{
+    int leastSoldProduct = -1;
+    int minQuantity = INT_MAX;
+    
+    // Check each product ID from 1 to maxStockId
+    for (int productId = 1; productId <= maxStockId; productId++) {
+        int totalQuantity = getProductTotalQuantitySold(productId);
+        
+        if (totalQuantity < minQuantity) {
+            minQuantity = totalQuantity;
+            leastSoldProduct = productId;
+        }
+    }
+    
+    return leastSoldProduct;
+}
+
+double SalesReport::getMostSoldProductTotalSales() const
+{
+    int mostSoldProductId = getMostSoldProductByQuantity();
+    return getProductTotalSales(mostSoldProductId);
+}
+
+double SalesReport::getTotalStockValue() const
+{
+    double totalValue = 0;
+    
+    for (const auto& stock : stockList) {
+        totalValue += stock.getQuantity() * stock.getCostValue();
+    }
+    
+    return totalValue;
+}
+
+void SalesReport::generateStockReport() const
+{    
+    double totalStockValue = 0;
+    
+    for (const auto& stock : stockList) {
+        double productTotal = stock.getQuantity() * stock.getCostValue();
+        totalStockValue += productTotal;
+    }
+    cout << "Total stock value: " << fixed << setprecision(2) << totalStockValue  << " eur." << endl;
+}
+
+void SalesReport::generateSalesReportByProduct(const string& productName) const
+{
+    cout << "Report for product: " << productName << endl;
+    
+    //HACK: this is AI shenenigans, needs to be redone as we have a method for this in store
+    // Find product ID by name
+    int productId = -1;
+    for (const auto& stock : stockList) {
+        if (stock.getProductName() == productName) {
+            productId = stock.getStockId();
+            break;
+        }
+    }
+    
+    if (productId == -1) {
+        cout << "Product not found!" << endl;
+        return;
+    }
+    
+    int totalQuantitySold = getProductTotalQuantitySold(productId);
+    double totalSalesValue = getProductTotalSales(productId);
+    
+    cout << "Product ID: " << productId << endl;
+    cout << "Product Name: " << productName << endl;
+    cout << "Total Quantity Sold: " << totalQuantitySold << endl;
+    cout << "Total Sales Value: " << fixed << setprecision(2) << totalSalesValue << " eur." << endl;
+    
+    // Show individual sales
+    cout << "\nIndividual Sales:" << endl;
+    cout << "Receipt ID | Quantity | Sale Value" << endl;
+    cout << "--------------------------------" << endl;
+    
+    for (int i = 0; i < receiptCount; i++) {
+        if (receipts[i].getReceiptId() > 0) {
+            for (const auto& item : receipts[i].getItems()) {
+                if (item.getStockId() == productId) {
+                    cout << receipts[i].getReceiptId() << " | " 
+                         << item.getQuantity() << " | " 
+                         << fixed << setprecision(2) << item.getTotalItemSellValue() << endl;
+                }
+            }
+        }
+    }
+}
+
+// Helper methods
+double SalesReport::getClientTotalPurchases(int clientId) const
+{
+    double total = 0;
+    for (int i = 0; i < receiptCount; i++) {
+        if (receipts[i].getReceiptId() > 0 && receipts[i].getClientId() == clientId) {
+            total += receipts[i].getPaymentAmount();
+        }
+    }
+    return total;
+}
+
+int SalesReport::getProductTotalQuantitySold(int stockId) const
+{
+    int total = 0;
+    for (int i = 0; i < receiptCount; i++) {
+        if (receipts[i].getReceiptId() > 0) {
+            for (const auto& item : receipts[i].getItems()) {
+                if (item.getStockId() == stockId) {
+                    total += item.getQuantity();
+                }
+            }
+        }
+    }
+    return total;
+}
+
+double SalesReport::getProductTotalSales(int stockId) const
+{
+    double total = 0;
+    for (int i = 0; i < receiptCount; i++) {
+        if (receipts[i].getReceiptId() > 0) {
+            for (const auto& item : receipts[i].getItems()) {
+                if (item.getStockId() == stockId) {
+                    total += item.getTotalItemSellValue();
+                }
+            }
+        }
+    }
+    return total;
+}
---
+++ test/new/salesReport.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <string>
+#include <array>
+#include <vector>
+#include "receipt.h"
+#include "stock.h"
+
+class SalesReport 
+{
+private:
+    array<Receipt, 100> receipts;
+    vector<Stock> stockList;
+    int receiptCount;
+    int maxStockId;
+    
+public:
+    SalesReport() : receiptCount(0), maxStockId(0) {}
+    
+    // Load receipts and stock data
+    void loadReceipts(const array<Receipt, 100>& saleList);
+    void loadStock(const vector<Stock>& stock);
+    
+    // Find client who bought the most in value
+    int getTopClientByValue() const;
+    
+    // Get the most sold product by quantity
+    int getMostSoldProductByQuantity() const;
+    
+    // Get the least sold product by quantity
+    int getLeastSoldProductByQuantity() const;
+    
+    // Get total sales value of most sold product
+    double getMostSoldProductTotalSales() const;
+    
+    // Calculate total stock value (sum of all products * their costValue)
+    double getTotalStockValue() const;
+    
+    // Generate stock report with total stock
+    void generateStockReport() const;
+    
+    // Sales report by product (receives product name as parameter)
+    void generateSalesReportByProduct(const string& productName) const;
+    
+    // Helper methods
+    double getClientTotalPurchases(int clientId) const;
+    int getProductTotalQuantitySold(int stockId) const;
+    double getProductTotalSales(int stockId) const;
+    int countValidReceipts() const;
+};
